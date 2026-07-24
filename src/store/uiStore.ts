759f3c7f7fe72288669d0f71ebcb72208d33e69f
Maham Saleem <mahamsaleem951@gmail.com>
2026-07-24 21:57:50 +0500

    feat: add isAuthOpen and pendingProduct to uiStore

--- src/store/uiStore.ts
+++ src/store/uiStore.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
+import type { Product } from '../data/products';
 
 interface UIStore {
   isCartOpen: boolean;
   isSearchOpen: boolean;
   isMobileMenuOpen: boolean;
+  isAuthOpen: boolean;
   openCart: () => void;
   closeCart: () => void;
   toggleCart: () => void;
@@ -11,12 +13,17 @@ interface UIStore {
   closeSearch: () => void;
   toggleMobileMenu: () => void;
   closeMobileMenu: () => void;
+  openAuth: () => void;
+  closeAuth: () => void;
+  pendingProduct: Product | null;
+  setPendingProduct: (p: Product | null) => void;
 }
 
 export const useUIStore = create<UIStore>((set) => ({
   isCartOpen: false,
   isSearchOpen: false,
   isMobileMenuOpen: false,
+  isAuthOpen: false,
   openCart: () => set({ isCartOpen: true }),
   closeCart: () => set({ isCartOpen: false }),
   toggleCart: () => set((s) => ({ isCartOpen: !s.isCartOpen })),
@@ -24,4 +31,8 @@ export const useUIStore = create<UIStore>((set) => ({
   closeSearch: () => set({ isSearchOpen: false }),
   toggleMobileMenu: () => set((s) => ({ isMobileMenuOpen: !s.isMobileMenuOpen })),
   closeMobileMenu: () => set({ isMobileMenuOpen: false }),
+  openAuth: () => set({ isAuthOpen: true }),
+  closeAuth: () => set({ isAuthOpen: false }),
+  pendingProduct: null,
+  setPendingProduct: (p) => set({ pendingProduct: p }),
 }));
